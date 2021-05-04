720199305c40c1be2d3fa7a3bc053a1c6483c3ff
Shinya Uryu <suika1127@gmail.com>
2021-05-04 16:18:08 +0900

    Fix abbreviation name

--- R/abbreviation.R
+++ R/abbreviation.R
@@ -4,9 +4,9 @@
 #' abbr_journal_name("Proceedings of the National Academy of Sciences")
 #' @export
 abbr_journal_name <- function(x) {
-  gsub(gsub("Journal", "J.",
+  gsub("Proceedings of the National Academy of Sciences", "PNAS",
+    gsub("Journal", "J.",
             gsub("\\{", "",
                  gsub("}", "",
-                      gsub("\\{\\\\", "", x)))),
-       "Proceedings of the National Academy of Sciences", "PNAS")
+                      gsub("\\{\\\\", "", x)))))
 }
